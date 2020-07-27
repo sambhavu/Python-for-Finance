3c38aa7683d4f96a5106ccd19c857b5e8719d680
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-07-27 13:59:02 -0400

    Create forking.py

---
+++ Shell/forking.py
@@ -0,0 +1,27 @@
+import os 
+
+def child(): 
+    print('\nA new child', os.getpid())
+    os.exit(0)
+    
+def parent():
+    while True: 
+        newpid = os.fork() 
+        if newpid == 0: 
+            child()
+        
+        else: 
+            pids = (os.getpid(), newpid)
+            print('parent: %d child: %d', % pids)
+            
+        reply = input("Q for quit / c for new fork")
+        if reply == 'c': 
+            continue 
+        else: 
+            break 
+        
+parent() 
+
+
+        
+                
