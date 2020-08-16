61884b8c1180647c373cb19195e882798dfd0937
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-08-15 21:28:57 -0400

    Create ft_.py

---
+++ ft_.py
@@ -0,0 +1,34 @@
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+data = yf.Ticker('AAPL')
+hist = data.history(period = '1y', interval = '1d')
+close = hist['Close'].values
+
+close = close.tolist()
+
+close.append(close[-1])
+
+close = np.asarray(close)
+
+
+
+
+close_fft = np.fft.fft(np.asarray(close.tolist()))
+close_fft_list = np.asarray(close_fft.tolist())
+
+num_ = 50
+fft_list_m10 = np.copy(close_fft_list); fft_list_m10[num_:-num_]=0
+ft = np.fft.ifft(fft_list_m10)
+
+
+close = close[-5:-1]
+ft = ft[-5:-1]
+
+print(close[-1])
+print(ft[-1])
+plt.plot(close)
+plt.plot(ft)
+plt.show()
