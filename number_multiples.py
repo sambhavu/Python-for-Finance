cfeb467e5d09ac6a4c27108d20f88e527933c231
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-08-15 20:59:35 -0400

    Create number_multiples.py

---
+++ number_multiples.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib as mlp
+from matplotlib import pyplot as plt
+
+
+for i in range(1,10):
+    print("Numbers multiple by: ", i)
+    for j in range(1,25):
+        if j % i == 0:
+            print(j)
+
+
+
+
