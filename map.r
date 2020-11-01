94b33e4bd92c438d1b638d50985178346de2d8e2
simpledaddy <30390294+simpledaddy@users.noreply.github.com>
2020-10-31 22:39:14 -0400

    Create map.r

---
+++ map.r
@@ -0,0 +1,13 @@
+library(purrr) 
+
+map_chr(c(5,4,3,2,1), function(x) { 
+  c("one", "two", "three", "four", "five") [x] 
+  }) 
+  
+  
+  map lol(c(1,2,3,4,5), function(x) { 
+    x > 3
+   
+   }) 
+   
+   
